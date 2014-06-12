cd43de8a7c0c3ec6e37f05231d0a414809a1149c
Eugene ONeill <oneill.eugene@gmail.com>
2014-06-12 13:16:05 -0700

    making view guids consistent

--- app/app.js
+++ app/app.js
@@ -4,7 +4,6 @@ import loadInitializers from 'ember/load-initializers';
 
 import EditTodoView from 'appkit/views/edit_todo';
 
-window.preRenderSyncPhase = true;
 (function(RenderBufferProto) {
   var _generateElement = RenderBufferProto.generateElement;
   var _element = RenderBufferProto.element;
@@ -30,6 +29,39 @@ window.preRenderSyncPhase = true;
     return window.preRenderSyncPhase ? this._element : _element.call(this);
   };
 }(Ember._RenderBuffer.prototype));
+
+(function() {
+  var o_defineProperty = Ember.platform.defineProperty;
+  var uuid = 0;
+  var GUID_KEY = Ember.GUID_KEY;
+  var GUID_DESC = {
+    writable:    false,
+    configurable: false,
+    enumerable:  false,
+    value: null
+  };
+
+  // this is a custom guid for views...
+  // @see Ember.guidFor
+  function guidForView(obj) {
+    var ret = 'emberv' + (uuid++);
+
+    if (obj[GUID_KEY] === null) {
+      obj[GUID_KEY] = ret;
+    } else {
+      GUID_DESC.value = ret;
+      o_defineProperty(obj, GUID_KEY, GUID_DESC);
+    }
+    return ret;
+  }
+
+  Ember.View.reopen({
+    init: function () {
+      this.elementId = this.elementId || guidForView(this);
+      this._super();
+    }
+  });
+}());
 
 var App = Ember.Application.extend({
   modulePrefix: 'appkit', // TODO: loaded via config
